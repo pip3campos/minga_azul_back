c73ebbe3b29dbfafa74aac7585d64e122e8de0d4
pip3campos <felipeacampose@gmail.com>
2023-09-17 18:10:32 -0300

    sprint3 mergeado completo

--- routes/chapters.js
+++ routes/chapters.js
@@ -6,14 +6,15 @@ import readChapter from "../controllers/chapters/readChapter.js";
 import addCoverPhoto from "../middlewares/add_cover_photo.js";
 import chapterValidator from "../validators/chapterValidator.js";
 import validator from "../middlewares/validator.js";
-//import passport from "../middlewares/passport.js";
+import passport from "../middlewares/passport.js";
+import is_property_of from "../middlewares/is_property_of.js"
 
 const router = Router();
 
 // router.get('/', read)
 router.get('/', readChapter)
 router.get('/:id', readOne)
-router.post('/',/*passport.authenticate('jwt', {session:false}), is_property_of,*/ addCoverPhoto, validator(chapterValidator) ,create)
+router.post('/',passport.authenticate('jwt', {session:false}), is_property_of, addCoverPhoto, validator(chapterValidator) ,create)
 
 
 export default router;
