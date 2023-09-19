ce7ba16299be0f2392ec82b415d3fc9c1ebeb11d
Martin <martinleonelgalan@gmail.com>
2023-09-19 11:20:31 -0300

    middlewares

---
+++ middlewares/is_active.js
@@ -0,0 +1,23 @@
+import Author from '../models/Author.js'
+
+const findAuthor = async (req, res, next) => {
+    try {
+        const findedAuthor = await Author.findById(req.body.id)
+        if (findedAuthor.active===true) {
+            return next()
+        }
+        return res.status(404).json({
+            success: false,
+            message: "Is Not Active",
+            response: null
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Error interno del servidor",
+            response: null
+        })
+    }
+}
+
+export default findAuthor
